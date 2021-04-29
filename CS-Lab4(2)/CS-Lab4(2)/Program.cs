20100e3968c405baf523237845934b130af7630f
Qurial <79192846+Qurial@users.noreply.github.com>
2021-04-29 13:44:00 +0300

    first commit

---
+++ CS-Lab4(2)/CS-Lab4(2)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CS_Lab4_2_
+{
+    class Program
+    {
+        [DllImport("MyLibrary.dll")]
+        public static extern double mPow(double a, double n);
+        static void Main(string[] args)
+        {
+            int a, n;
+            Console.Write("Input a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Result is: ");
+            double result = MyLibrary.Calculations.mPow(a, n);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
